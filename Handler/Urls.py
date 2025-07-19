3678b4c5a497349707d5fbcd0a8047253728325d
Abdul Aminu Tahiru <96601052+Think-IT-think-gui@users.noreply.github.com>
2025-07-19 09:15:49 +0000

    Update Urls.py

--- Handler/Urls.py
+++ Handler/Urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from . views import Pay_Transaction
+from . views import *
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     
     path('pay_transaction', Pay_Transaction.as_view(), name="pay_transaction"),
+    path('transaction', Transaction.as_view(), name="transaction"),
+    
     
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
