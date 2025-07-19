3842f09cf27f74b511db120d4effc45764ab9be2
Abdul Aminu Tahiru <96601052+Think-IT-think-gui@users.noreply.github.com>
2025-07-19 10:25:02 +0000

    Update Urls.py

--- Handler/Urls.py
+++ Handler/Urls.py
@@ -5,6 +5,19 @@ from django.conf import settings
 urlpatterns = [
     
     path('pay_transaction', Pay_Transaction.as_view(), name="pay_transaction"),
+    path('main_transaction', Transfere_Account.as_view(), name="main_transaction"),
+    path('recure_transaction', New_Recure.as_view(), name="recure_transaction"),
+    path('check_ref', Check_Ref.as_view(), name="check_ref"),
+    path('delete_transaction', Delete_transaction.as_view(), name="delete_transaction"),
+    path('initialize_transaction', initialize_transaction.as_view(), name="initialize_transaction"),
+    path('chech_number', Chech_Number.as_view(), name="chech_number"),    
+    path('check_prepiad', Check_Prepaid.as_view(), name="check_prepaid"),
+    path('check_card', Check_Card.as_view(), name="check_card"),
+    path('check_bank', Check_Bank.as_view(), name="check_bank"),
+    path('check_meter', Check_Meter.as_view(), name="check_meter"),
+    path('send_bank', Send_Bank.as_view(), name="send_bank"),
+    path('initialize_ecg', Initialize_Ecg.as_view(), name="initialize_ecg"),
+
     path('transaction', Transaction.as_view(), name="transaction"),
     
     
